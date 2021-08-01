03eafe4d8a4c5eca8b8e5a77ec11a55719eea79a
WannaBe2D <nikolayskochko03@gmail.com>
2021-08-01 19:10:46 +0200

    push

--- src/App.js
+++ src/App.js
@@ -12,24 +12,24 @@ import Orders from './pages/Orders';
 
 function App() {
 
-  const[items, setItems] = React.useState([]);
+  const [items, setItems] = React.useState([]);
 
-  const[cartOpened, setCartOpened] = React.useState(false);
+  const [cartOpened, setCartOpened] = React.useState(false);
 
-  const[searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState('');
 
-  const[cartItems, setCartItems] = React.useState([]);
-  const[favoriteItems, setFavorite] = React.useState([]);
-  const[orderId, setOrderId]  = React.useState(18);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [favoriteItems, setFavorite] = React.useState([]);
+  const [orderId, setOrderId] = React.useState(18);
 
-  const[isLoading, setIsLoading] = React.useState(true);
-  
+  const [isLoading, setIsLoading] = React.useState(true);
+
   React.useEffect(() => {
     async function fetchData() {
       const itemsResponse = await axios.get("https://mdatest2.herokuapp.com/api/react_sneakers/");
       const cartResponse = await axios.get("https://mdatest2.herokuapp.com/api/react_cart/");
       const favoriteResponse = await axios.get("https://mdatest2.herokuapp.com/api/react_favorite/");
-      
+
       setIsLoading(false);
       setCartItems(cartResponse.data['0']['sneakers']);
       setFavorite(favoriteResponse.data['0']['sneakers']);
@@ -45,42 +45,42 @@ function App() {
 
   const addToFovarite = async (id) => {
     try {
-      if(favoriteItems.find(e => e.id === id)){
+      if (favoriteItems.find(e => e.id === id)) {
         axios.delete(`https://mdatest2.herokuapp.com/api/react_favorite/`, {
           data: {
             "id": id
           }
         })
         setFavorite(prev => prev.filter((e) => e.id !== id))
-      }else{
+      } else {
         const { data } = await axios.post("https://mdatest2.herokuapp.com/api/react_favorite/", {
           "id": id
         })
         setFavorite(prev => [...prev, data])
       }
-    } catch (error){
+    } catch (error) {
       alert('Не удалось добавить в фавориты');
     }
   }
 
   const addToCart = async (id) => {
-   try {
-    if(cartItems.find(e => e.id === id)){
-      axios.delete(`https://mdatest2.herokuapp.com/api/react_cart/`, {
-        data: {
-          "id": id
-        }
-      })
-      setCartItems(prev => prev.filter((e) => e.id !== id))
-      }else {
+    try {
+      if (cartItems.find(e => e.id === id)) {
+        axios.delete(`https://mdatest2.herokuapp.com/api/react_cart/`, {
+          data: {
+            "id": id
+          }
+        })
+        setCartItems(prev => prev.filter((e) => e.id !== id))
+      } else {
         const { data } = await axios.post("https://mdatest2.herokuapp.com/api/react_cart/", {
           "id": id
-        }) 
+        })
         setCartItems(prev => [...prev, data])
       }
-   } catch(error) {
-    alert('Не удалось добавить в корзину');
-   }
+    } catch (error) {
+      alert('Не удалось добавить в корзину');
+    }
   }
 
   const removeItemCart = (id) => {
@@ -104,46 +104,46 @@ function App() {
     setCartOpened(false)
   }
 
-  const createOrder = async(id) => {
+  const createOrder = async (id) => {
     try {
       const { data } = await axios.post('https://mdatest2.herokuapp.com/api/react_order/', {
         "sneakers": id
       })
       setCartItems([]);
       setOrderId(data.id)
-    }catch(error){
+    } catch (error) {
       alert('Не удалось сделать заказ')
     }
   }
 
   return (
-    <AppContext.Provider value={{ items, cartItems, favoriteItems, isItemAdded, addToFovarite, isItemFavorited, onClose, createOrder, orderId}}>
-    <div className="wrapper clear">
-      {cartOpened && <Drawer onRemove={removeItemCart} items={cartItems}  />}
-      <Header onClickCart={() => setCartOpened(true)} />
+    <AppContext.Provider value={{ items, cartItems, favoriteItems, isItemAdded, addToFovarite, isItemFavorited, onClose, createOrder, orderId }}>
+      <div className="wrapper clear">
+        {cartOpened && <Drawer onRemove={removeItemCart} items={cartItems} />}
+        <Header onClickCart={() => setCartOpened(true)} />
 
-      <Route path="/favorites">
-        <Favorites
-          addToCart={addToCart} 
-        />
-      </Route>
+        <Route path="/favorites">
+          <Favorites
+            addToCart={addToCart}
+          />
+        </Route>
 
-      <Route path="/orders">
-        <Orders />
-      </Route>
+        <Route path="/orders">
+          <Orders />
+        </Route>
 
-      <Route path="/" exact>
-        <Home
-          searchValue={searchValue}
-          setSearchValue={setSearchValue}
-          onChangeValue={onChangeValue}
-          items={items}
-          addToCart={addToCart}
-          cartItems={cartItems}
-          isLoading={isLoading}
-         />
-      </Route>      
-    </div>
+        <Route path="" exact>
+          <Home
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            onChangeValue={onChangeValue}
+            items={items}
+            addToCart={addToCart}
+            cartItems={cartItems}
+            isLoading={isLoading}
+          />
+        </Route>
+      </div>
     </AppContext.Provider>
   );
 }
